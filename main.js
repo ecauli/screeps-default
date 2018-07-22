4f9823bbd5b70c3e805e6d032a37b02bfa8092e0
Eduardo Cauli <ecauli@gmail.com>
2018-07-22 11:32:55 -0300

    number of harvester 5

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ module.exports.loop = function () {
     }
 
     // setup some minimum numbers for different roles
-    var minimumNumberOfHarvesters = 4;
+    var minimumNumberOfHarvesters = 5;
     var minimumNumberOfUpgraders = 1;
     var minimumNumberOfBuilders = 1;
     var minimumNumberOfRepairers = 2;
